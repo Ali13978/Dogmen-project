4d4d567abbbf7573cbbc4473805a628caf8e70aa
Ali13978 <aliwebconsultant@gmail.com>
2024-03-10 14:07:20 +0500

    Dawg man traffic update

--- Assets/Scripts/DogSelection/DogSelectionHelper.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DogSelectionHelper : MonoBehaviour
-{
-    [SerializeField] public int IndexInList;
-}
